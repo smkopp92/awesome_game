bc5b2b1bf5bcfa1edfbd6a51fed97eed7c2ec791
Sebastian Kopp <smkopp92@gmail.com>
2017-09-07 18:38:00 -0400

    implement replayability

--- game.rb
+++ game.rb
@@ -1,11 +1,23 @@
-number = rand(20)
+playing = true
+while playing do
+  number = rand(20)
 
-puts "Guess a number between 1 and 20"
+  puts "Guess a number between 1 and 20"
 
-answer = gets.chomp
+  answer = gets.chomp
 
-if answer.to_i == number
-  puts "You win!"
-else
-  puts "You lose"
+  if answer.to_i == number
+    puts "You win!"
+  else
+    puts "You lose"
+  end
+
+  puts "Do you want to play again? (Y/N)"
+
+  answer = gets.chomp
+
+  if answer == "N"
+    playing = false
+    puts "Cya nerd"
+  end
 end
